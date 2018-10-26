0919bd2f1b05fc30e356178c676c8a9616b6f9a5
EYoung75 <eyoung75.ey@gmail.com>
2018-10-26 10:16:23 -0600

    fetch request from image API and rendered on page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Image from "./components/images.jsx";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      images: []
+    }
+  }
+
+  async componentDidMount() {
+    const response = await fetch("https://picsum.photos/200/300/?random)")
+    //const json = await response.json()
+    this.setState({images: response.url})
+    console.log(this.state.images)
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Image images={this.state.images}/>
       </div>
     );
   }
